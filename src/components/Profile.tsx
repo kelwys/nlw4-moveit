736a078278d687da96e5fbb58badd60c90021b96
Kelwy <kelwyduarte@gmail.com>
2021-02-27 02:01:29 -0300

    Implementa contexto de countdown

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengsContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+    
     return(
         <div className={styles.profileContainer}>
             <img src="https://github.com/kelwys.png" alt="Kelwy Oliveira"/>
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Kelwy Oliveira</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1 
+                    Level {level}
                 </p>
             </div>
         </div>
